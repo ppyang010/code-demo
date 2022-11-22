5acec0b568384307566f9fce7f29f867734a49bd
caicy <caicy@dxy.cn>
2022-11-22 20:12:49 +0800

    2022年11月22日20:12:39

---
+++ springdemo/src/test/java/com/code/other/DefaultTest.java
@@ -0,0 +1,17 @@
+package com.code.other;
+
+import cn.hutool.core.lang.tree.TreeUtil;
+
+/**
+ *
+ * 默认测试代码
+ * 未确定分类的测试/验证代码都可以用这个
+ * @author ccy
+ * @description
+ * @time 2022/11/22 18:02
+ */
+public class DefaultTest {
+
+    public static void main(String[] args) {
+    }
+}
